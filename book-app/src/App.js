8dac5065537e22d01d1256e280f0ddeacdcb6ca2
David Vincent <70216205+jazzyboii@users.noreply.github.com>
2022-06-07 14:29:59 -0400

    workspace stash

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -3,24 +3,13 @@ import { Routes, Route, Link } from "react-router-dom";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import BookPage from "./components/bookPage/BookPage";
 import AuthorContextProvider from "./contexts/authorContext";
+import HomePage from "./components/homePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <AuthorContextProvider>
-        <nav>
-          <p>
-            <Link to="/shopping-cart">Shopping Cart</Link>
-          </p>
-          <p>
-            <Link to="/book-info">Book Info</Link>
-          </p>
-        </nav>
-
-        <Routes>
-          <Route path="/shopping-cart" element={<ShoppingCart />} />
-          <Route path="/book-info" element={<BookPage />} />
-        </Routes>
+        <HomePage/>
       </AuthorContextProvider>
     </div>
   );
