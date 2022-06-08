e9e779221e0e3cd3553f540fcd8ceddb344a2ce0
Devang Ray <devangray624@gmail.com>
2022-06-08 11:02:32 -0400

    added submit feature

--- book-app/src/components/ShoppingCart/PaymentPage.js
+++ book-app/src/components/ShoppingCart/PaymentPage.js
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import {Grid, TextField, Button, Select, InputLabel, MenuItem} from "@mui/material";
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 import { ShoppingContext } from "../../contexts/shoppingContext";
 import {useNavigate} from 'react-router-dom';
 import './paymentPage.css'
@@ -147,7 +148,7 @@ export default function PaymentPage() {
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(formValues);
-        setFormValues(defaultValues);
+        // setFormValues(defaultValues);
         setDataSubmitted(true);
     };
 
@@ -194,7 +195,112 @@ export default function PaymentPage() {
             <div className="parent">
             <div className="div2"><p>This is the zipcode</p></div>
             <div className='div1'>
-                        <Paper elevation={5}>
+                <Paper elevation={5}>
+                    {dataSubmitted?<form onSubmit={handleSubmit}>
+                            <Grid container spacing={2} alignItems="center" direction="column">
+                                <Grid item xs={12} sm={4}>
+                                    <TextField
+                                        required
+                                        variant="filled" color="success"
+                                        id="first-name-input"
+                                        name="first"
+                                        label="First Name"
+                                        type="text"
+                                        value={formValues.first}
+                                        onChange={handleFirstInputChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={4}>
+                                    <TextField
+                                        required
+                                        variant="filled" color="success"
+                                        id="last-name-input"
+                                        name="last"
+                                        label="Last Name"
+                                        type="text"
+                                        value={formValues.last}
+                                        onChange={handleLastInputChange}
+                                    />
+                                </Grid>
+                                 <Grid item>
+                                    <TextField
+                                        required
+                                        variant="filled" color="success"
+                                        id="email-input"
+                                        name="email"
+                                        label="Email Address"
+                                        type="text"
+                                        value={formValues.email}
+                                        onChange={handleEmailInputChange}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <InputLabel id="demo-simple-select-label">Expiration Date</InputLabel>
+                                    <Select
+                                        required
+                                        variant="filled" color="success"
+                                        id="expiration-month-input"
+                                        name="expirationMonth"
+                                        value={formValues.expirationMonth}
+                                        label="Month"
+                                        onChange={handleInputChange}
+                                    >
+                                        <MenuItem value={1}>1 - January</MenuItem>
+                                        <MenuItem value={2}>2 - February</MenuItem>
+                                        <MenuItem value={3}>3 - March</MenuItem>
+                                        <MenuItem value={4}>4 - April</MenuItem>
+                                        <MenuItem value={5}>5 - May</MenuItem>
+                                        <MenuItem value={6}>6 - June</MenuItem>
+                                        <MenuItem value={7}>7 - July</MenuItem>
+                                        <MenuItem value={8}>8 - August</MenuItem>
+                                        <MenuItem value={9}>9 - September</MenuItem>
+                                        <MenuItem value={10}>10 - October</MenuItem>
+                                        <MenuItem value={11}>11 - November</MenuItem>
+                                        <MenuItem value={12}>12 - December</MenuItem>
+                                    </Select>
+
+                                    <TextField
+                                        required
+                                        variant="filled" color="success"
+                                        id="expiration-year-input"
+                                        name="expirationYear"
+                                        label="Expiration Year"
+                                        type="text"
+                                        value={formValues.expirationYear}
+                                        onChange={handleExpirationYearInputChange}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        variant="filled" color="success"
+                                        id="creditCardNumber-input"
+                                        name="creditCardNum"
+                                        label="Credit Card Number"
+                                        type="text"
+                                        value={formValues.creditCardNum}
+                                        onChange={handleCreditCardNumberInputChange}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        variant="filled" color="success"
+                                        id="cvc-input"
+                                        name="cvc"
+                                        label="CVV"
+                                        type="number"
+                                        value={formValues.cvc}
+                                        onChange={handleCVCInputChange}
+                                    />
+                                </Grid>
+                                <div className='submitButton'>
+                                    <Button variant="contained" disabled color="primary" type="submit" className='addStudentbutton'>
+                                        Submit
+                                    </Button>
+                                </div>
+                            </Grid>
+                            </form>:
                         <form onSubmit={handleSubmit}>
                             <Grid container spacing={2} alignItems="center" direction="column">
                                 <Grid item xs={12} sm={4}>
@@ -360,7 +466,8 @@ export default function PaymentPage() {
                                 </div>
                             </Grid>
                             </form>
-                        </Paper>
+                        }
+                </Paper>
             </div>
             <div className="div5"><Button onClick={goBack}>Go Back</Button></div>
             <div className="div4"><p>Price breakdown</p></div>
